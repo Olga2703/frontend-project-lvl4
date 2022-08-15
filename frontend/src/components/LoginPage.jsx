717208c363ce32b282584502dd5b26404c19e12d
Olga Petrenko <smirnova_olga18@mail.ru>
2022-08-15 22:45:02 +0400

    fix: login page

--- frontend/src/components/LoginPage.jsx
+++ frontend/src/components/LoginPage.jsx
@@ -4,27 +4,34 @@ import { Formik, Form, Field } from 'formik';
 import avatar from '../assets/avatar.jpg';
 
 const LoginPage = () => (
-    <div className="container">
-        <div className="row">
-            <div className="col-6">
-                <img src={avatar} alt="avatar" className="rounded-circle" />
-            </div>
-            <Formik
-                initialValues = { {
-                  login: '',
-                  password: '',
-                }}
-            >
+    <div className="container-fluid h-100 mt-5">
+        <div className="row justify-content-center align-content-center h-100">
+            <div className="col-12 col-md-8 col-xxl-6">
+                <div className="card shadow-sm">
+                    <div className="card-body row p-5">
+                        <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+                            <img src={avatar} alt="avatar" className="rounded-circle" />
+                        </div>
+                        <Formik
+                            initialValues = { {
+                              login: '',
+                              password: '',
+                            }}
+                        >
+                        {() => (
+                            <Form className="col-12 col-md-6 mt-3 mt-mb-0">
+                                <h1 className="text-center mb-4">Войти</h1>
+                                <Field type='name' name='login' placeholder="Ваш ник" className="form-floating mb-4"/>
+                                <Field type='password' name='password' placeholder="Пароль" />
+                                <button type='submit'>Войти</button>
+                            </Form>)
+                        }
+                        </Formik>
+                    </div>
+                </div>
 
-               {() => (
-                <Form>
-                    <Field type='name' name='login' placeholder="Ваш ник" />
-                    <Field type='password' name='password' placeholder="Пароль" />
-                    <button type='submit'>Войти</button>
-                </Form>
-               )
-               }
-            </Formik>
+            </div>
+
         </div>
         <div className="row">
 
