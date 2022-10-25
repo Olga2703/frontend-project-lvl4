4bb6587a06f9b28474d27ff8bfd833a276e19c2d
Olga Petrenko <smirnova_olga18@mail.ru>
2022-10-26 00:31:06 +0400

    feat: add modalWindow

---
+++ frontend/src/components/ModalWindow.jsx
@@ -0,0 +1,84 @@
+import React, { useRef, useEffect, useContext } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Button, Modal, Form } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { selectors } from '../slices/channelsSlice.js';
+import { actions as modalActions } from '../slices/modalsSlice.js';
+import { ApiContext } from '../context/index.js';
+
+const ModalWindow = () => {
+  const chatApi = useContext(ApiContext);
+  const dispatch = useDispatch();
+  const inputRef = useRef();
+  useEffect(() => inputRef.current.focus(), []);
+
+  const channelNames = useSelector(selectors.selectAll).map((channel) => channel.name);
+
+  const closeModal = () => dispatch(modalActions.closeModal());
+
+  const { type, channel } = useSelector((state) => state.modals);
+
+  const onAdd = (name) => {
+    try {
+      chatApi.addNewChannel({ removable: true, name });
+    } catch (err) {}
+  };
+
+  const onRename = (name) => {
+    try {
+      chatApi.renameChannel({ id: channel.id, name });
+    } catch (err) {}
+  };
+
+  const title = type === 'adding' ? 'добавить канал' : 'переименовать канал';
+  const handler = type === 'adding' ? onAdd : onRename;
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+    },
+    validationSchema: yup.object({
+      name: yup.string().required().notOneOf(channelNames),
+    }),
+    onSubmit: ({ name }) => {
+      handler(name);
+      closeModal();
+    },
+  });
+
+  return (
+    <Modal show centered>
+      <Modal.Header closeButton onHide={closeModal}>
+        <Modal.Title className='h4'>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={formik.handleSubmit}>
+          <div>
+            <Form.Control
+              isInvalid={formik.touched.name && formik.errors.name}
+              ref={inputRef}
+              onChange={formik.handleChange}
+              className='mb-2'
+              name='name'
+              id='name'
+              value={formik.values.name}
+            />
+            <Form.Label className='visually-hidden' htmlFor='name'>
+              {'Имя канала'}
+            </Form.Label>
+            {formik.touched.name && formik.errors.name ? <Form.Control.Feedback type='invalid'>{formik.errors.name}</Form.Control.Feedback> : null}
+            <div className='d-flex justify-content-end'>
+              <Button onClick={closeModal} type='button' variant='secondary' className='me-2'>
+                {'отменить'}
+              </Button>
+              <Button type='submit'>{'Отправить'}</Button>
+            </div>
+          </div>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ModalWindow;
--- frontend/src/components/PrivatePage.jsx
+++ frontend/src/components/PrivatePage.jsx
@@ -12,6 +12,7 @@ import ChannelButton from './ChannelButtons.jsx';
 import MessagesForm from './MessagesForm.jsx';
 import { selectors as messagesSelectors } from '../slices/messagesSlice.js';
 import { useAuth } from '../hooks/index.js';
+import { actions } from '../slices/modalsSlice.js';
 
 const PrivatePage = () => {
   const dispatch = useDispatch();
@@ -25,6 +26,7 @@ const PrivatePage = () => {
   const messages = useSelector(messagesSelectors.selectAll);
   const { currentChannelId } = useSelector((state) => state.channels);
   const currentMessages = messages.filter((message) => message.channelId === currentChannelId);
+  const showAddingModal = () => dispatch(actions.openModal({ type: 'adding', channel: null}))
 
   return (
     <div className="d-flex flex-column h-100">
@@ -33,7 +35,7 @@ const PrivatePage = () => {
         <Col xs={4} md={2} className='border-end pt-5 px-0 bg-light'>
           <div className='d-flex justify-content-between mb-2 ps-4 pe-2'>
             <span>Каналы</span>
-            <Button variant='' className='p-0 btn-group-vertical text-primary'>
+            <Button onClick={showAddingModal} variant='' className='p-0 btn-group-vertical text-primary'>
               <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='20' height='20' fill='currentColor'>
                 <path d='M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z' />
                 <path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z' />
