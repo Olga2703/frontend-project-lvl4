1333b7964e0df38f0b134e168db4f59cb72ed5d4
Olga Petrenko <smirnova_olga18@mail.ru>
2022-12-01 11:25:08 +0400

    fix: rollbar workflows and and deploy to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
+
 [![Actions Status](https://github.com/Olga2703/frontend-project-lvl4/workflows/hexlet-check/badge.svg)](https://github.com/Olga2703/frontend-project-lvl4/actions)
+
+https://frontend-project-lvl4-production-389b.up.railway.app/
--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,19 +3,34 @@ import { Provider } from 'react-redux';
 import { io } from 'socket.io-client';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
-import { Provider as ProviderRollbar, ErrorBoundary } from '@rollbar/react';
+import { Provider as ProviderRollbar, ErrorBoundary, LEVEL_WARN } from '@rollbar/react';
 import ru from './locales/ru.js';
 import App from './components/App.jsx';
 import store from './slices/index.js';
 import buildChatApi from './api/buildChatApi.js';
+import NotFoundPage from './components/NotFoundPage.jsx';
 import { actions as messagesActions } from './slices/messagesSlice.js';
 import { actions as channelActions } from './slices/channelsSlice.js';
 import { ApiContext } from './context/index.js';
 
+const ErrorBoundaryPage = () => <NotFoundPage />;
+
 const init = () => {
   const rollbarConfig = {
-    accessToken: process.env.TOKEN,
-    payload: 'production',
+    enabled: process.env.NODE_ENV === 'production',
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    payload: {
+      environment: 'production',
+      client: {
+        javascript: {
+          source_map_enabled: true,
+          code_version: '0.0.1',
+          guess_uncaught_frames: true,
+        },
+      },
+    },
   };
 
   const i18nextInstance = i18n.createInstance();
@@ -52,7 +67,7 @@ const init = () => {
 
   return (
     <ProviderRollbar config={rollbarConfig}>
-      <ErrorBoundary>
+      <ErrorBoundary level={LEVEL_WARN} fallbackUI={ErrorBoundaryPage}>
         <Provider store={store}>
           <ApiContext.Provider value={chatApi}>
             <I18nextProvider i18n={i18nextInstance}>
