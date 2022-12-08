1ee4001b0374e29fd2ed8c9637714a65e3d9df66
Olga Petrenko <smirnova_olga18@mail.ru>
2022-12-08 14:39:45 +0400

    fix: function authProvider move own file

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -14,41 +13,8 @@ import NotFoundPage from './NotFoundPage.jsx';
 import PrivatePage from './PrivatePage.jsx';
 import Registration from './Registration.jsx';
 import routes from '../routes.js';
-import { AuthContext } from '../context/index.js';
 import { useAuth } from '../hooks/index.js';
-
-const AuthProvider = ({ children }) => {
-  const currentUser = JSON.parse(localStorage.getItem('user'));
-  const [user, setUser] = useState(currentUser ? { username: currentUser.username } : null);
-  const logIn = (userData) => {
-    localStorage.setItem('user', JSON.stringify(userData));
-    setUser({ username: userData.username });
-  };
-
-  const logOut = () => {
-    localStorage.removeItem('user');
-    setUser(null);
-  };
-
-  const getAuthHeader = () => {
-    const userData = JSON.parse(localStorage.getItem('user'));
-
-    return userData?.token ? { Authorization: `Bearer ${userData.token}` } : {};
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{
-        logIn,
-        logOut,
-        getAuthHeader,
-        user,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
-};
+import AuthProvider from '../api/AuthProvider.jsx';
 
 const PrivateOutlet = () => {
   const auth = useAuth();
