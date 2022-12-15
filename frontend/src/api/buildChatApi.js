28d973e6ab8aa2fd89d8e2c093d40602e7cd966e
Olga Petrenko <smirnova_olga18@mail.ru>
2022-12-15 15:59:10 +0400

    fix: update buildChatApi

--- frontend/src/api/buildChatApi.js
+++ frontend/src/api/buildChatApi.js
@@ -1,41 +1,65 @@
+import { actions as messagesActions } from "../slices/messagesSlice.js";
+import { actions as channelActions } from "../slices/channelsSlice.js";
+
 const buildChatApi = (socket) => {
   const sendMessage = (newMessage) => {
-    socket.emit('newMessage', newMessage, (response) => {
+    socket.emit("newMessage", newMessage, (response) => {
       console.log(response);
-      if (response.status !== 'ok') {
-        throw new Error('Network error: message delivery failed');
+      if (response.status !== "ok") {
+        throw new Error("Network error: message delivery failed");
       }
     });
   };
 
   const addNewChannel = (newChannel) => {
-    socket.emit('newChannel', newChannel, (response) => {
-      if (response.status !== 'ok') {
-        throw new Error('Network error: channel adding failed');
+    socket.emit("newChannel", newChannel, (response) => {
+      if (response.status !== "ok") {
+        throw new Error("Network error: channel adding failed");
       }
     });
   };
 
   const removeChannel = (channel) => {
-    socket.emit('removeChannel', channel, (response) => {
-      if (response.status !== 'ok') {
-        throw new Error('Network error: channel removing failed');
+    socket.emit("removeChannel", channel, (response) => {
+      if (response.status !== "ok") {
+        throw new Error("Network error: channel removing failed");
       }
     });
   };
   const renameChannel = (channel) => {
-    socket.emit('renameChannel', channel, (response) => {
-      if (response.status !== 'ok') {
-        throw new Error('Network error: channel renaming failed');
+    socket.emit("renameChannel", channel, (response) => {
+      if (response.status !== "ok") {
+        throw new Error("Network error: channel renaming failed");
       }
     });
   };
+
+  const getNewMessage = (dispatch) => (
+    socket.on("newMessage", (response) => dispatch(messagesActions.addMessage(response))));
+
+  const getNewChannel = (dispatch) => (
+    socket.on("newChannel", (response) => {
+      dispatch(channelActions.addChannel(response));
+      dispatch(channelActions.setCurrentChannelId(response));
+    }));
+
+  const getRemoveChannel = (dispatch) => (
+    socket.on("removeChannel", (response) => (
+      dispatch(channelActions.removeChannel(response.id)))));
+
+  const getRenameChannel = (dispatch) => (
+    socket.on("renameChannel", ({ id, name }) => (
+      dispatch(channelActions.updateChannel({ id, changes: { name } })))));
 
   return {
     sendMessage,
     addNewChannel,
     removeChannel,
     renameChannel,
+    getNewMessage,
+    getNewChannel,
+    getRemoveChannel,
+    getRenameChannel
   };
 };
 
--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -9,10 +9,7 @@ import App from './components/App.jsx';
 import store from './slices/index.js';
 import buildChatApi from './api/buildChatApi.js';
 import NotFoundPage from './components/notFoundPage/NotFoundPage.jsx';
-import { actions as messagesActions } from './slices/messagesSlice.js';
-import { actions as channelActions } from './slices/channelsSlice.js';
 import { ApiContext } from './context/index.js';
-
 const ErrorBoundaryPage = () => <NotFoundPage />;
 
 const init = () => {
@@ -48,22 +45,10 @@ const init = () => {
   const socket = io();
   const chatApi = buildChatApi(socket);
 
-  socket.on('newMessage', (response) => {
-    store.dispatch(messagesActions.addMessage(response));
-  });
-
-  socket.on('newChannel', (response) => {
-    store.dispatch(channelActions.addChannel(response));
-    store.dispatch(channelActions.setCurrentChannelId(response.id));
-  });
-
-  socket.on('removeChannel', (response) => {
-    store.dispatch(channelActions.removeChannel(response.id));
-  });
-
-  socket.on('renameChannel', ({ id, name }) => {
-    store.dispatch(channelActions.updateChannel({ id, changes: { name } }));
-  });
+  chatApi.getNewMessage(store.dispatch);
+  chatApi.getNewChannel(store.dispatch);
+  chatApi.getRemoveChannel(store.dispatch);
+  chatApi.getRenameChannel((store.dispatch));
 
   return (
     <ProviderRollbar config={rollbarConfig}>
