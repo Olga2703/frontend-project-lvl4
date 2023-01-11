388ef728343f075f2c15972637316f1798ce92ce
Olga Petrenko <smirnova_olga18@mail.ru>
2023-01-11 13:35:30 +0400

    fix: component Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,23 +6,18 @@ import routes from '../routes';
 
 const Header = () => {
   const { t } = useTranslation();
-  const AuthButton = () => {
-    const auth = useAuth();
-    return auth.user
-      && (
-      <Button href={routes.loginPagePath()} onClick={auth.logOut}>
-        {t('navbar.btn_out')}
-      </Button>
-      );
-  };
+  const auth = useAuth();
   return (
     <Navbar bg="white" expand="lg" className="shadow-sm">
       <div className="container">
         <Navbar.Brand href="/">{t('navbar.header')}</Navbar.Brand>
-        <AuthButton />
+        {auth.user && (
+          <Button href={routes.loginPagePath()} onClick={auth.logOut}>
+            {t('navbar.btn_out')}
+          </Button>
+        )}
       </div>
     </Navbar>
   );
 };
-
 export default Header;
