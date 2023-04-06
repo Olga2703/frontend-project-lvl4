235822d3cb1f68f40838f406924ff12ab32ca4ce
Olga Petrenko <smirnova_olga18@mail.ru>
2023-04-06 14:11:20 +0400

    fix

--- frontend/src/components/chatPage/components/modal/Modal.jsx
+++ frontend/src/components/chatPage/components/modal/Modal.jsx
@@ -25,3 +25,16 @@ const Modal = () => {
 };
 
 export default Modal;
+
+
+function getIndex(str, firstLetter, secondLetter) {
+  let i = str.length - 1;
+  while (i > 0) {
+    let letter = str.charAt(i);
+    if (letter === firstLetter || letter === secondLetter) {
+      return i;
+    }
+    i -= 1;
+  }
+  return -1;
+}
